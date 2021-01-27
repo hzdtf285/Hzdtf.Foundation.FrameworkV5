6d181875b495f1d0ac7e5316494d63a588c8cdb3
huangandy <hzdtf285@126.com>
2021-01-27 21:52:01 +0800

    update

--- src/Example/Application/Hzdtf.Example.WebApp/Startup.cs
+++ src/Example/Application/Hzdtf.Example.WebApp/Startup.cs
@@ -1,24 +1,17 @@
 using Autofac;
 using Hzdtf.BasicFunction.Controller.Extensions.RoutePermission;
-using Hzdtf.BasicFunction.Model;
 using Hzdtf.Example.WebApp.AppStart;
 using Hzdtf.Logger.Integration.ENLog;
 using Hzdtf.Utility;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text.Encodings.Web;
-using System.Text.Unicode;
-using System.Threading.Tasks;
 
 namespace Hzdtf.Example.WebApp
 {
