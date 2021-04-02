482d76e847d0e3cc8f21ff4284eeb9bab87e228f
huangandy <hzdtf285@126.com>
2021-04-02 15:29:39 +0800

    update

---
+++ src/Persistence/Hzdtf.EntityFrameworkCore.Extensions/ScalarModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    /// <summary>
+    /// 首位数据模型
+    /// @ 黄振东
+    /// </summary>
+    /// <typeparam name="T">数据类型</typeparam>
+    public class ScalarModel<T>
+    {
+        /// <summary>
+        /// 结果
+        /// </summary>
+        public T Result
+        {
+            get;
+            set;
+        }
+    }
+}
