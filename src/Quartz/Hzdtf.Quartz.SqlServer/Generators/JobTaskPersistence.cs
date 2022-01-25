930ea4160717f22b5e83bd3487d23d4369ae88fb
hzdtf <hzdtf@PC-HUANGZHENDON>
2022-01-26 03:28:32 +0800

    添加时钟sqlserver的支持

---
+++ src/Quartz/Hzdtf.Quartz.SqlServer/Generators/JobTaskPersistence.cs
@@ -0,0 +1,139 @@
+﻿﻿using Hzdtf.Quartz.Model;
+using Hzdtf.SqlServer;
+using Hzdtf.Utility.Attr;
+using System;
+using Hzdtf.Quartz.Persistence.Contract;
+
+namespace Hzdtf.Quartz.SqlServer
+{
+    /// <summary>
+    /// 作业任务持久化
+    /// @ 黄振东
+    /// </summary>
+    [Inject]
+    public partial class JobTaskPersistence : SqlServerDapperBase<int, JobTaskInfo>, IJobTaskPersistence
+    {
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public override string Table => "job_task";
+
+        /// <summary>
+        /// 插入字段名称集合
+        /// </summary>
+        private readonly static string[] INSERT_FIELD_NAMES = new string[]
+        {
+            "create_time",
+            "id",
+            "job_full_class",
+            "job_params_json_string",
+            "jt_desc",
+            "jt_group",
+            "jt_name",
+            "modify_time",
+            "successed_remove",
+            "trigger_cron",
+            "trigger_params_json_string",
+        };
+
+        /// <summary>
+        /// 更新字段名称集合
+        /// </summary>
+        private readonly static string[] UPDATE_FIELD_NAMES = new string[]
+        {
+            "job_full_class",
+            "job_params_json_string",
+            "jt_desc",
+            "jt_group",
+            "jt_name",
+            "modify_time",
+            "successed_remove",
+            "trigger_cron",
+            "trigger_params_json_string",
+        };
+
+        /// <summary>
+        /// 所有字段映射集合
+        /// </summary>
+        private readonly static string[] FIELD_MAP_PROPS = new string[]
+        {
+            "create_time CreateTime",
+            "id Id",
+            "job_full_class JobFullClass",
+            "job_params_json_string JobParamsJsonString",
+            "jt_desc JtDesc",
+            "jt_group JtGroup",
+            "jt_name JtName",
+            "modify_time ModifyTime",
+            "successed_remove SuccessedRemove",
+            "trigger_cron TriggerCron",
+            "trigger_params_json_string TriggerParamsJsonString",
+        };
+
+        /// <summary>
+        /// 根据字段名获取模型的值
+        /// </summary>
+        /// <param name="model">模型</param>
+        /// <param name="field">字段名</param>
+        /// <returns>值</returns>
+        protected override object GetValueByFieldName(JobTaskInfo model, string field)
+        {
+            switch (field)
+            {
+﻿                case "create_time":
+                    return model.CreateTime;
+
+﻿                case "id":
+                    return model.Id;
+
+﻿                case "job_full_class":
+                    return model.JobFullClass;
+
+﻿                case "job_params_json_string":
+                    return model.JobParamsJsonString;
+
+﻿                case "jt_desc":
+                    return model.JtDesc;
+
+﻿                case "jt_group":
+                    return model.JtGroup;
+
+﻿                case "jt_name":
+                    return model.JtName;
+
+﻿                case "modify_time":
+                    return model.ModifyTime;
+
+﻿                case "successed_remove":
+                    return model.SuccessedRemove;
+
+﻿                case "trigger_cron":
+                    return model.TriggerCron;
+
+﻿                case "trigger_params_json_string":
+                    return model.TriggerParamsJsonString;
+
+                default:
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// 插入字段名集合
+        /// </summary>
+        /// <returns>插入字段名集合</returns>
+        protected override string[] InsertFieldNames() => INSERT_FIELD_NAMES;
+
+        /// <summary>
+        /// 更新字段名称集合
+        /// </summary>
+        /// <returns>更新字段名称集合</returns>
+        protected override string[] UpdateFieldNames() => UPDATE_FIELD_NAMES;
+
+		/// <summary>
+        /// 所有字段映射集合
+        /// </summary>
+        /// <returns>所有字段映射集合</returns>
+        public override string[] AllFieldMapProps() => FIELD_MAP_PROPS;
+    }
+}
---
+++ src/Quartz/Hzdtf.Quartz.SqlServer/JobTaskPersistence.cs
@@ -0,0 +1,395 @@
+﻿using Dapper;
+using Hzdtf.Persistence.Contract.Management;
+using Hzdtf.Quartz.Model;
+using Hzdtf.Quartz.Persistence.Contract;
+using Hzdtf.Utility.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hzdtf.Utility.Utils;
+using Hzdtf.Utility.Model.Page;
+using System.Text;
+
+namespace Hzdtf.Quartz.SqlServer
+{
+    /// <summary>
+    /// 作业任务持久化
+    /// @ 黄振东
+    /// </summary>
+    public partial class JobTaskPersistence : IJobTaskBasicPersistence
+    {
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="filter">过滤器</param>
+        /// <param name="connectionId">连接ID</param>
+        /// <returns>作业任务信息列表</returns>
+        public IList<JobTaskInfo> Query(JobTaskFilterInfo filter = null, string connectionId = null)
+        {
+            var sql = $"{BasicSelectSql()} {GetWhereSql(ref filter)}";
+            if (!string.IsNullOrWhiteSpace(filter.SortName))
+            {
+                sql += $" ORDER BY {PfxEscapeChar}{filter.SortName}{SufxEscapeChar} {filter.Sort.ToString()}";
+            }
+
+            IList<JobTaskInfo> result = null;
+            DbConnectionManager.BrainpowerExecute(connectionId, this, (connId, dbConn) =>
+            {
+                result = ExecRecordSqlLog<IList<JobTaskInfo>>(sql, () =>
+                {
+                    return dbConn.Query<JobTaskInfo>(sql, filter, GetDbTransaction(connId, AccessMode.SLAVE)).ToList();
+                }, tag: "Query");
+            }, AccessMode.SLAVE);
+            DbToModelFullOtherProps(result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// 查询分页
+        /// </summary>
+        /// <param name="pageIndex">页码，从0开始</param>
+        /// <param name="pageSize">每页记录数</param>
+        /// <param name="filter">过滤器</param>
+        /// <param name="connectionId">连接ID</param>
+        /// <returns>分页信息</returns>
+        public PagingInfo<JobTaskInfo> QueryPage(int pageIndex, int pageSize, JobTaskFilterInfo filter = null, string connectionId = null)
+        {
+            var whereSql = GetWhereSql(ref filter);
+            if (string.IsNullOrWhiteSpace(filter.SortName))
+            {
+                filter.SortName = "CreateTime";
+                filter.Sort = SortType.ASC;
+            }
+
+            var orderSql = new StringBuilder($"ORDER BY {PfxEscapeChar}{filter.SortName}{SufxEscapeChar} {filter.Sort.ToString()}");
+            if (!"Id".Equals(filter.SortName))
+            {
+                orderSql.AppendFormat(",{0}", GetFieldByProp("Id"));
+            }
+
+            var whereSqlStr = whereSql.ToString();
+            var countSql = $"{BasicCountSql()} {whereSqlStr}";
+            var selectSql = $"{BasicSelectSql()} {whereSqlStr} {orderSql.ToString()} {GetPartPageSql(pageIndex, pageSize)}";
+
+            PagingInfo<JobTaskInfo> result = null;
+            DbConnectionManager.BrainpowerExecute(connectionId, this, (connId, dbConn) =>
+            {
+                var trans = GetDbTransaction(connId, AccessMode.SLAVE);
+                result = PagingUtil.ExecPage<JobTaskInfo>(pageIndex, pageSize, () =>
+                {
+                    return ExecRecordSqlLog<int>(countSql, () =>
+                    {
+                        return dbConn.ExecuteScalar<int>(countSql, filter, transaction: trans);
+                    }, tag: "QueryPage");
+                }, () =>
+                {
+                    return ExecRecordSqlLog<IList<JobTaskInfo>>(selectSql, () =>
+                    {
+                        return dbConn.Query<JobTaskInfo>(selectSql, filter, transaction: trans).AsList();
+                    }, tag: "QueryPage");
+                });
+            }, AccessMode.SLAVE);
+            DbToModelFullOtherProps(result.Rows);
+
+            return result;
+        }
+
+        /// <summary>
+        /// 获取条件SQL
+        /// </summary>
+        /// <param name="filter">过滤器</param>
+        /// <returns>条件SQL</returns>
+        private string GetWhereSql(ref JobTaskFilterInfo filter)
+        {
+            var sql = CreateWhereSql();
+            if (filter == null)
+            {
+                filter = new JobTaskFilterInfo();
+                return sql.ToString();
+            }
+
+            if (!string.IsNullOrWhiteSpace(filter.Keyword))
+            {
+                sql.AppendFormat(" AND ({0}{1}{2} LIKE '%{3}%' OR {0}{4}{2} LIKE '%{3}%')", 
+                    PfxEscapeChar, GetFieldByProp("JtName"), SufxEscapeChar, filter.Keyword.FillSqlValue(), GetFieldByProp("JtGroup"));
+            }
+            if (filter.StartCreateTime != null)
+            {
+                sql.AppendFormat(" AND {0}{1}{2}>=@StartCreateTime", PfxEscapeChar, GetFieldByProp("CreateTime"), SufxEscapeChar);
+            }
+            if (filter.EndCreateTime != null)
+            {
+                sql.AppendFormat(" AND {0}{1}{2}>=@EndCreateTime", PfxEscapeChar, GetFieldByProp("CreateTime"), SufxEscapeChar);
+            }
+
+            return sql.ToString();
+        }
+
+        /// <summary>
+        /// 查找
+        /// </summary>
+        /// <param name="name">名称</param>
+        /// <param name="group">分组</param>
+        /// <param name="connectionId">连接ID</param>
+        /// <returns>作业任务</returns>
+        public JobTaskInfo Find(string name, string group = null, string connectionId = null)
+        {
+            var sql = $"{BasicSelectSql()} WHERE {GetNameAndGroupSql()}";
+            JobTaskInfo result = null;
+            DbConnectionManager.BrainpowerExecute(connectionId, this, (connId, dbConn) =>
+            {
+                result = ExecRecordSqlLog<JobTaskInfo>(sql, () =>
+                {
+                    return dbConn.QueryFirstOrDefault<JobTaskInfo>(sql, new
+                    {
+                        Name = name,
+                        Group = group
+                    }, GetDbTransaction(connId, AccessMode.SLAVE));
+                }, tag: "Find");
+            }, AccessMode.SLAVE);
+            DbToModelFullOtherProps(result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// 查找
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <param name="connectionId">连接ID</param>
+        /// <returns>作业任务</returns>
+        public JobTaskInfo Find(int id, string connectionId = null)
+        {
+            var sql = $"{BasicSelectSql()} WHERE {PfxEscapeChar}{GetFieldByProp("Id")}{SufxEscapeChar}=@Id";
+            JobTaskInfo result = null;
+            DbConnectionManager.BrainpowerExecute(connectionId, this, (connId, dbConn) =>
+            {
+                result = ExecRecordSqlLog<JobTaskInfo>(sql, () =>
+                {
+                    return dbConn.QueryFirstOrDefault<JobTaskInfo>(sql, new
+                    {
+                        Id = id,
+                    }, GetDbTransaction(connId, AccessMode.SLAVE));
+                }, tag: "Find");
+            }, AccessMode.SLAVE);
+            DbToModelFullOtherProps(result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// 是否存在
+        /// </summary>
+        /// <param name="name">名称</param>
+        /// <param name="group">分组</param>
+        /// <param name="connectionId">连接ID</param>
+        /// <returns>作业任务</returns>
+        public bool Exists(string name, string group = null, string connectionId = null)
+        {
+            var sql = $"{BasicCountSql()} WHERE {GetNameAndGroupSql()}";
+            int result = 0;
+            DbConnectionManager.BrainpowerExecute(connectionId, this, (connId, dbConn) =>
+            {
+                result = ExecRecordSqlLog<int>(sql, () =>
+                {
+                    return dbConn.ExecuteScalar<int>(sql, new
+                    {
+                        Name = name,
+                        Group = group
+                    }, GetDbTransaction(connId, AccessMode.SLAVE));
+                }, tag: "Exists");
+            }, AccessMode.SLAVE);
+
+            return result > 0;
+        }
+
+        /// <summary>
+        /// 是否存在
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <param name="connectionId">连接ID</param>
+        /// <returns>作业任务</returns>
+        public bool Exists(int id, string connectionId = null)
+        {
+            var sql = $"{BasicCountSql()} WHERE {PfxEscapeChar}{GetFieldByProp("Id")}{SufxEscapeChar}=@Id";
+            int result = 0;
+            DbConnectionManager.BrainpowerExecute(connectionId, this, (connId, dbConn) =>
+            {
+                result = ExecRecordSqlLog<int>(sql, () =>
+                {
+                    return dbConn.ExecuteScalar<int>(sql, new
+                    {
+                        Id = id,
+                    }, GetDbTransaction(connId, AccessMode.SLAVE));
+                }, tag: "Exists");
+            }, AccessMode.SLAVE);
+
+            return result > 0;
+        }
+
+        /// <summary>
+        /// 设置，如果存在则更新，否则插入
+        /// </summary>
+        /// <param name="jobTask">作业任务</param>
+        /// <param name="connectionId">连接ID</param>
+        /// <returns>影响行数</returns>
+        public int Set(JobTaskInfo jobTask, string connectionId = null)
+        {
+            int result = 0;
+            ModelToDbFullOtherProps(jobTask);
+            DbConnectionManager.BrainpowerExecute(connectionId, this, (connId, dbConn) =>
+            {
+                if (Exists(jobTask.JtName, jobTask.JtGroup, connId))
+                {
+                    result = UpdateCron(jobTask.TriggerCron, jobTask.JtName, jobTask.JtGroup, connId);
+                }
+                else
+                {
+                    jobTask.CreateTime = jobTask.ModifyTime = DateTime.Now;
+                    result = Insert(jobTask, connectionId: connId);
+                }
+            });
+
+            return result;
+        }
+
+        /// <summary>
+        /// 更新Cron表达式
+        /// </summary>
+        /// <param name="cron">cron表达式</param>
+        /// <param name="name">名称</param>
+        /// <param name="group">分组</param>
+        /// <param name="connectionId">连接ID</param>
+        /// <returns>影响行数</returns>
+        public int UpdateCron(string cron, string name, string group = null, string connectionId = null)
+        {
+            var sql = $"UPDATE {PfxEscapeChar}{Table}{SufxEscapeChar} SET {PfxEscapeChar}{GetFieldByProp("TriggerCron")}{SufxEscapeChar}=@TriggerCron,{PfxEscapeChar}{GetFieldByProp("ModifyTime")}{SufxEscapeChar}=NOW()" +
+                $" WHERE {GetNameAndGroupSql()}";
+            int result = 0;
+            DbConnectionManager.BrainpowerExecute(connectionId, this, (connId, dbConn) =>
+            {
+                result = ExecRecordSqlLog<int>(sql, () =>
+                {
+                    return dbConn.Execute(sql, new
+                    {
+                        TriggerCron = cron,
+                        Name = name,
+                        Group = group
+                    }, GetDbTransaction(connId));
+                }, tag: "UpdateCron");
+            });
+
+            return result;
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <param name="connectionId">连接ID</param>
+        /// <returns>影响行数</returns>
+        public int Delete(int id, string connectionId = null)
+        {
+            return this.DeleteById(id, connectionId: connectionId);
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="name">名称</param>
+        /// <param name="group">分组</param>
+        /// <param name="connectionId">连接ID</param>
+        /// <returns>影响行数</returns>
+        public int Delete(string name, string group = null, string connectionId = null)
+        {
+            var sql = $"{BasicDeleteSql()} WHERE {GetNameAndGroupSql()}";
+            int result = 0;
+            DbConnectionManager.BrainpowerExecute(connectionId, this, (connId, dbConn) =>
+            {
+                result = ExecRecordSqlLog<int>(sql, () =>
+                {
+                    return dbConn.Execute(sql, new
+                    {
+                        Name = name,
+                        Group = group
+                    }, GetDbTransaction(connId));
+                }, tag: "Delete");
+            });
+
+            return result;
+        }
+
+        /// <summary>
+        /// 数据库到模型填充其他属性
+        /// </summary>
+        /// <param name="jobTasks">作业任务列表</param>
+        private void DbToModelFullOtherProps(IList<JobTaskInfo> jobTasks)
+        {
+            if (jobTasks.IsNullOrCount0())
+            {
+                return;
+            }
+
+            foreach (var jt in jobTasks)
+            {
+                DbToModelFullOtherProps(jt);
+            }
+        }
+
+        /// <summary>
+        /// 数据库到模型填充其他属性
+        /// </summary>
+        /// <param name="jobTask">作业任务</param>
+        private void DbToModelFullOtherProps(JobTaskInfo jobTask)
+        {
+            if (jobTask == null)
+            {
+                return;
+            }
+            jobTask.JobParams = jobTask.JobParamsJsonString.ToJsonObject<IDictionary<string, string>>();
+            jobTask.TriggerParams = jobTask.TriggerParamsJsonString.ToJsonObject<IDictionary<string, string>>();
+        }
+
+        /// <summary>
+        /// 模型到数据库填充其他属性
+        /// </summary>
+        /// <param name="jobTasks">作业任务列表</param>
+        private void ModelToDbFullOtherProps(IList<JobTaskInfo> jobTasks)
+        {
+            if (jobTasks.IsNullOrCount0())
+            {
+                return;
+            }
+
+            foreach (var jt in jobTasks)
+            {
+                ModelToDbFullOtherProps(jt);
+            }
+        }
+
+        /// <summary>
+        /// 模型到数据库填充其他属性
+        /// </summary>
+        /// <param name="jobTask">作业任务</param>
+        private void ModelToDbFullOtherProps(JobTaskInfo jobTask)
+        {
+            if (jobTask == null)
+            {
+                return;
+            }
+            jobTask.JobParamsJsonString = jobTask.JobParams.ToJsonString();
+            jobTask.TriggerParamsJsonString = jobTask.TriggerParams.ToJsonString();
+        }
+
+        /// <summary>
+        /// 获取名称和分组SQL
+        /// </summary>
+        /// <returns>名称和分组SQL</returns>
+        private string GetNameAndGroupSql()
+        {
+            return $"{PfxEscapeChar}{GetFieldByProp("JtName")}{SufxEscapeChar}=@Name AND" +
+                $" {PfxEscapeChar}{GetFieldByProp("JtGroup")}{SufxEscapeChar}=@Group";
+        }
+    }
+}
